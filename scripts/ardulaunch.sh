3c58467cd16c5691c43d0cf7d7089cf7a60cdaae
Dheeraj Mohan <dheeraj.98@outlook.com>
2022-11-14 04:01:36 -0800

    Implement cooperative launching based on destination of drones

--- scripts/ardulaunch.sh
+++ scripts/ardulaunch.sh
@@ -1,3 +1,3 @@
 
     #!/bin/bash
-    konsole --new-tab -e "sim_vehicle.py -v ArduCopter -f gazebo-drone1 -I0" --new-tab -e "sim_vehicle.py -v ArduCopter -f gazebo-drone2 -I1" 
+    konsole --hold--new-tab -e "sim_vehicle.py -v ArduCopter -f gazebo-drone1 -I0" --new-tab -e "sim_vehicle.py -v ArduCopter -f gazebo-drone2 -I1" 
--- src/square.cpp
+++ src/square.cpp
@@ -1,4 +1,10 @@
 #include <gnc_functions.hpp>
+#include <std_msgs/Bool.h>
+
+// void prevTrackerCallback(const std_msgs::Bool::ConstPtr& msg){
+//     ROS_INFO("I heard: [%d]", msg->data);
+// }
+
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "drone_hq");
@@ -12,11 +18,40 @@ int main(int argc, char** argv){
     hq.getParam("height", height);
 
     std::cout<<"ID: "<<id<<", Height: "<<height<<std::endl;
-
     wait4connect();
     // wait4start();
     set_mode("GUIDED");
     initialize_local_frame();
+    
+    if(id > 1){
+        bool prevReached = false;
+        auto prevTrackerCallback = [&prevReached](const std_msgs::Bool::ConstPtr& msg){ prevReached = msg->data;};
+
+        char buffer[100];
+        sprintf(buffer, "/iris%d/square%d/moving", id-1, id-1);
+        std::string topic(buffer);
+        ros::Subscriber prevSucceeded = hq.subscribe<std_msgs::Bool>(topic, 100, prevTrackerCallback);
+        ros::Rate rateWaiter(2.0);
+        while(prevReached == false){
+            ros::spinOnce();
+            rateWaiter.sleep();
+            continue;
+        }
+    }
+        
+    ros::Publisher succeededTopic = hq.advertise<std_msgs::Bool>("moving", 100);
     takeoff(height);
+    ros::Rate rate(2.0);
+    bool reached = false;
+    while(ros::ok()){
+        ros::spinOnce();
+        rate.sleep();
+        std_msgs::Bool moving;
+        moving.data = reached;
+        succeededTopic.publish(moving);
+        if(check_waypoint_reached()==1){
+            reached = true;
+        }
+    }
 
 }
