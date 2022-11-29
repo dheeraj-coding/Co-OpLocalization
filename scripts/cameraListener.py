462454262ad833cc48bcbba0e44a1a6e96d48d33
Dheeraj Mohan <dheeraj.98@outlook.com>
2022-11-28 18:54:39 -0800

    Implement pose transformation in cameraListener to obtain point data

--- scripts/cameraListener.py
+++ scripts/cameraListener.py
@@ -6,16 +6,20 @@ from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
 from pyzbar.pyzbar import decode
 import numpy as np
+import tf
+from geometry_msgs.msg import PointStamped
 
 import cv2
 
 bridge = CvBridge()
 MARKER_LENGTH = 0.18
+tfListener = None
 
 def callback(image, camera_info):
     # rospy.loginfo(rospy.get_caller_id()+"I heard %s", data.data)
     print("Received Image")
     global MARKER_LENGTH
+    global tfListener
     try:
         cv2_img = bridge.imgmsg_to_cv2(image, "bgr8")
     except CvBridgeError as e:
@@ -49,12 +53,44 @@ def callback(image, camera_info):
                 point = cv2.drawFrameAxes(cv2_img, cam_mat, camera_info.D, rVec[i], tVec[i], 1, 1)
                 cv2_img = cv2.putText(cv2_img, "id: {id} Dist: {dist: .2f}".format(id=id[0], dist=distance), tuple(top_right), cv2.FONT_HERSHEY_PLAIN, 1.3, (0, 0, 255), 2, cv2.LINE_AA)
                 cv2_img = cv2.putText(cv2_img, f"x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)} z: {round(tVec[i][0][2], 1)}", tuple(bottom_right), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255), 2,cv2.LINE_AA)
+                
+                id = id[0]
+                drone_id = id // 5
+                box_id = id % 5
+                pt = PointStamped()
+                pt.header.stamp = rospy.Time(0)
+                if box_id == 0:
+                    pt.header.frame_id = "iris"+str(drone_id)+"_front"
+                elif box_id == 1:
+                    pt.header.frame_id = "iris"+str(drone_id)+"_right"
+                elif box_id == 2:
+                    pt.header.frame_id = "iris"+str(drone_id)+"_left"
+                elif box_id == 3:
+                    pt.header.frame_id = "iris"+str(drone_id)+"_back"
+                elif box_id == 4:
+                    pt.header.frame_id = "iris"+str(drone_id)+"_top"
+                print(pt.header.frame_id)
+                pt.point.x = tVec[i][0][0]
+                pt.point.y = tVec[i][0][1]
+                pt.point.z = tVec[i][0][2]
+
+                try:
+                    resP = tfListener.transformPoint("map", pt)
+                    print("Drone is at:")
+                    print(resP)
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+                    print("Transform error")
+                    print(e)
+                    continue
+
+
 
         cv2.imshow('droneImg', cv2_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
 def listener():
+    global tfListener
     rospy.init_node('listener', anonymous=True)
     # rospy.Subscriber('chatter', String, callback)
     # rospy.spin()
@@ -63,10 +99,11 @@ def listener():
     id = int(id)
     print("ID= ", id)
 
+    tfListener = tf.TransformListener()
+
     image_sub = message_filters.Subscriber('iris{id}/image_raw'.format(id=id), Image)
     info_sub = message_filters.Subscriber('iris{id}/camera_info'.format(id=id), CameraInfo)
 
-    
 
     ts = message_filters.TimeSynchronizer([image_sub, info_sub], 10)
     ts.registerCallback(callback)
