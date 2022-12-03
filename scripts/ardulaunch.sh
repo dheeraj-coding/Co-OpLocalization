0e243e98239476d988b52b389a95226ea1e81358
neha-chawla <nehaxchawla@gmail.com>
2022-12-03 12:43:43 -0800

    pyramid

--- scripts/ardulaunch.sh
+++ scripts/ardulaunch.sh
@@ -1,3 +1,3 @@
 
     #!/bin/bash
-    konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone1 -I0" &konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone2 -I1" &konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone3 -I2" &konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone4 -I3" &
+    konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone1 -I0" &konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone2 -I1" &konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone3 -I2" &konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone4 -I3" &konsole --hold --new-tab -e $SHELL -c "sim_vehicle.py -v ArduCopter -f gazebo-drone5 -I4" &
