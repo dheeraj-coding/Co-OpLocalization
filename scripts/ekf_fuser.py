dbd2bd86abda25d49d6c7d90d228dd2bfda96451
Dheeraj Mohan <dheeraj.98@outlook.com>
2022-12-03 20:23:09 -0800

    Implement time synchronized ekf_fuser node

---
+++ scripts/ekf_fuser.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import rospy
+import message_filters
+from sensor_msgs.msg import Image, CameraInfo
+from nav_msgs.msg import Odometry
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+import tf
+import cv2
+
+MARKER_LENGTH = 0.18
+
+class EKFfusion:
+    def __init__(self):
+        rospy.init_node('ekf_fuser', anonymous=True)
+        self.bridge = CvBridge()
+        id = rospy.get_param("~id")
+        self.id = int(id)
+        
+        image_sub = message_filters.Subscriber('/iris{id}/image_raw'.format(id=id), Image)
+        cam_info = message_filters.Subscriber('/iris{id}/camera_info'.format(id=id), CameraInfo)
+        odom_sub = message_filters.Subscriber('/iris{id}/mavros/local_position/odom'.format(id=id), Odometry)
+
+        self.tfListener = tf.TransformListener()
+
+        ts = message_filters.ApproximateTimeSynchronizer([image_sub, cam_info, odom_sub], 10, 0.5)
+        ts.registerCallback(self.handleSynchronizedCallback)
+        rospy.spin()
+    
+    def processImage(self, img, camInfo):
+        arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_1000)
+        arucoParams = cv2.aruco.DetectorParameters_create()
+        grayframe = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        (corners, ids, _) = cv2.aruco.detectMarkers(grayframe, arucoDict, parameters=arucoParams)
+        if corners:
+            camMat = np.asarray(camInfo.K)
+            camMat = camMat.reshape(3, 3)
+            rVecList, tVecList, _ = cv2.aruco.estimatePoseSingleMarkers(corners, MARKER_LENGTH, camMat, camInfo.D)
+            totalMarkers = range(0, ids.size)
+            for id, corner, i in zip(ids, corners, totalMarkers):
+                self.drawAxes(id, img, corner, tVecList[i], rVecList[i], camMat, camInfo.D)
+                continue
+        return img
+    
+    def drawAxes(self, id, img, corner, tVec, rVec, intrinsic, distortion):
+        cv2.polylines(img, [corner.astype(np.int32)], True, (0, 255, 255), 1, cv2.LINE_AA)
+        corner = corner.reshape(4, 2)
+        corner = corner.astype(int)
+        top_right = corner[0].ravel()
+        bottom_right = corner[2].ravel()
+        distance = np.sqrt(tVec[0][0]**2+tVec[0][1]**2+tVec[0][2]**2)
+        _ = cv2.drawFrameAxes(img, intrinsic, distortion, rVec, tVec, 1, 1)
+        img = cv2.putText(img, "id: {id} Dist: {dist: .2f}".format(id=id, dist=distance), tuple(top_right), cv2.FONT_HERSHEY_PLAIN, 1.3, (0, 0, 255), 2, cv2.LINE_AA)
+        img = cv2.putText(img, f"x:{round(tVec[0][0], 1)} y:{round(tVec[0][1], 1)} z:{round(tVec[0][2], 1)}", tuple(bottom_right), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255), 2, cv2.LINE_AA)
+
+        return img
+
+    def handleSynchronizedCallback(self, img, camInfo, odom):
+
+        try:
+            cv2img = self.bridge.imgmsg_to_cv2(img, 'bgr8')
+            processed = self.processImage(cv2img, camInfo)
+            cv2.imshow('droneImg', processed)
+            cv2.waitKey(100)
+            cv2.destroyAllWindows()
+        except CvBridgeError as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    fuser = EKFfusion()
