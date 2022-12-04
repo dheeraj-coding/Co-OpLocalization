0548c3e40d0f043c171aa56e4ad50c11060e597f
Dheeraj Mohan <dheeraj.98@outlook.com>
2022-12-03 19:05:44 -0800

    Implement position service for obtaining position information

---
+++ scripts/positionClient.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+from coop_localization.srv import *
+import time
+
+POSITION_SERVICE = 'position'
+STAGNANT_SERVICE = 'stagnant'
+
+def retrieve_pos(time):
+    rospy.wait_for_service(POSITION_SERVICE)
+    try:
+        positionRequest = rospy.ServiceProxy(POSITION_SERVICE, PositionInfo)
+        resp = positionRequest(rospy.Time(0))
+        print(rospy.Time(0)) 
+        print(resp)
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+def retrieve_stag():
+    rospy.wait_for_service(POSITION_SERVICE)
+    try:
+        stagRequest = rospy.ServiceProxy(STAGNANT_SERVICE, Stagnant)
+        resp = stagRequest(True)
+        print(resp)
+    except rospy.ServiceException as e:
+        print("Stag Service call failed: %s"%e)
+
+if __name__ == "__main__":
+    retrieve_stag()
+    time.sleep(2)
+    retrieve_pos(rospy.Time(0))
---
+++ scripts/positionService.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import Odometry
+import message_filters
+from coop_localization.srv import PositionInfo, PositionInfoResponse
+from coop_localization.srv import Stagnant, StagnantResponse
+
+class PositionService:
+    def __init__(self) -> None:
+        rospy.init_node('positionNode', anonymous=True)
+        
+        self.stagnant = False
+
+        id = rospy.get_param("~id")
+        self.id = int(id)
+        myTopic = '/iris{id}/mavros/local_position/odom'.format(id=id)
+
+        odomSub = message_filters.Subscriber(myTopic, Odometry)
+        self.cache = message_filters.Cache(odomSub, 500)
+    
+        rospy.Service('position', PositionInfo, self.handlePositionRequest)
+        rospy.Service('stagnant', Stagnant, self.handleStagnantRequest)
+        rospy.spin()
+
+    def handleStagnantRequest(self, status):
+        self.stagnant = status.stagnant
+        return StagnantResponse(True)
+        
+    
+    def handlePositionRequest(self, req):
+        pos = self.cache.getElemBeforeTime(req.stamp)
+        stationary = self.stagnant
+        return PositionInfoResponse(pos, stationary)
+if __name__ == "__main__":
+    srv = PositionService()
