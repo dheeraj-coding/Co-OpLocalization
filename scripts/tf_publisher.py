aa20b82d5f0c67f731fe3b01065485cd13f2af03
Dheeraj Mohan <dheeraj.98@outlook.com>
2022-12-06 00:51:29 -0800

    Fix odom tf publisher

--- scripts/tf_publisher.py
+++ scripts/tf_publisher.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import PoseStamped
+from tf.transformations import *
 import time
 import tf
+import math
+import numpy as np
 
 class DroneTfPublisher:
     def __init__(self, id) -> None:
         self.id = id
         self.drone_name = 'iris{id}'.format(id=id)
     def handle_drone_pose(self, pose_msg):
+        q_rot = quaternion_from_euler(0, 0, -math.pi/2.0)
+        q_orig = np.asarray([pose_msg.pose.orientation.x, pose_msg.pose.orientation.y, pose_msg.pose.orientation.z, pose_msg.pose.orientation.w])
+        q_new = quaternion_multiply(q_rot, q_orig)
         br = tf.TransformBroadcaster()
         br.sendTransform((pose_msg.pose.position.x, pose_msg.pose.position.y, pose_msg.pose.position.z),
-                    (pose_msg.pose.orientation.x, pose_msg.pose.orientation.y, pose_msg.pose.orientation.z, pose_msg.pose.orientation.w),
+                    (q_new[0], q_new[1], q_new[2], q_new[3]),
                     rospy.Time.now(),
                     self.drone_name,
                     self.drone_name+"_odom")
